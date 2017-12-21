fab192fac9a052ad4fb2b2bf28436e55fd027627
lmucassi <lindasmucassi@gmai.com>
2017-12-21 15:16:33 +0200

    arr len

--- src/checker.c
+++ src/checker.c
@@ -6,7 +6,7 @@
 /*   By: lmucassi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/29 14:35:31 by lmucassi          #+#    #+#             */
-/*   Updated: 2017/12/21 14:43:49 by lmucassi         ###   ########.fr       */
+/*   Updated: 2017/12/21 15:15:14 by lmucassi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,19 +18,25 @@ int		main(int ac, char **av)
 	t_stack *b;
 	t_stack	*head;
 	char **str;
+	int i;
+	
 	
 	if (ac == 2)
 	{
+		i = 1;
+		b = NULL;
 		ft_putstr("Working with one ARG string\n");
 		str = ft_strsplit(av[1], ' ');
-	
-		size_t i = multdi_len(str);
-		/*	size_t i = ft_strlen(*str);*/
-		while (i > 0)
-		{
+		
+		a = ft_new_stack(ft_atoi(str[0]));
+		while (*str)
+		{	
 			ft_putstr(*str++);
+			a = ft_new_stack(ft_atoi(str[i]));
 			ft_putstr("\n");
+			i++;
 		}
+		ft_printer(a, b);
 	}
 	else if (ac > 1)
 	{
